c88338e052cb8c79d651793eff0643bf1f070f6f
Malte Schledjewski <mschledjewski@aol.com>
2013-10-25 11:28:57 +0200

    added index.html with leaflet.js (without the api-key). added a sample overlay.js

---
+++ overlay.js
@@ -0,0 +1,6 @@
+var line1 = L.polyline([[49.2769, 7.2953], [49.2075, 7.4352]], {
+                weight: 0,
+                color: 'orange',
+                opacity: 0
+            }).addTo(map);
+line1.setText('\u25BA  1 \u25BA \u25BA', {repeat: true,offset: 6,attributes: {fill: 'black'}});
