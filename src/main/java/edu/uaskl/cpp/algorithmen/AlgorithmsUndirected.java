4d87925331b1ee744b3b5d1f351aff9aebe73fc1
tbachcern <tbach@cern.ch>
2013-11-06 07:38:31 -0800

    update warnings update unit tests

--- src/main/java/edu/uaskl/cpp/algorithmen/AlgorithmsUndirected.java
+++ src/main/java/edu/uaskl/cpp/algorithmen/AlgorithmsUndirected.java
@@ -21,248 +21,184 @@ public class AlgorithmsUndirected implements Algorithms {
         return graph;
     }
 
-	public boolean isConnected()
-	{
-				
-		graph.resetStates();
-		
-		if(graph.getNodes().size()==0) return true;
-		
-		NodeCpp start = graph.getNodes().iterator().next();
-				
-		visitAllEdgesFromStartNode(start);
-		
+    public boolean isConnected() {
 
-		for (NodeCpp nodeItem : graph.getNodes()) {
-			for (EdgeCpp edgeItem : nodeItem.getEdges()) {
-				if(edgeItem.isVisited()== false)
-					return false;
-			}
-		}
-		
-		return true;
-		
-	}
-	
-	private void visitAllEdgesFromStartNode(NodeCpp node)
-	{
+        graph.resetStates();
 
-		for (EdgeCpp edgeItem : node.getEdges()) {
-			
-			if(edgeItem.isVisited()==true) 
-				return;
-			edgeItem.setVisited();
-			visitAllEdgesFromStartNode(edgeItem.getNode2());
-		}
-	}
+        if (graph.getNodes().size() == 0)
+            return true;
+
+        final NodeCpp start = graph.getNodes().iterator().next();
+
+        visitAllEdgesFromStartNode(start);
+
+        for (final NodeCpp nodeItem : graph.getNodes())
+            for (final EdgeCpp edgeItem : nodeItem.getEdges())
+                if (edgeItem.isVisited() == false)
+                    return false;
+
+        return true;
+
+    }
+
+    private void visitAllEdgesFromStartNode(final NodeCpp node) {
+
+        for (final EdgeCpp edgeItem : node.getEdges()) {
+
+            if (edgeItem.isVisited() == true)
+                return;
+            edgeItem.setVisited();
+            visitAllEdgesFromStartNode(edgeItem.getNode2());
+        }
+    }
 
     public boolean hasEulerCircle() {
-		for (NodeCpp nodesItem : graph.getNodes()) {
-			if (nodesItem.isDegreeOdd())
-				return false;
-		}
-		return true;
+        for (final NodeCpp nodesItem : graph.getNodes())
+            if (nodesItem.isDegreeOdd())
+                return false;
+        return true;
     }
-    
-	private boolean isDeadEnd(NodeCpp node)
-	{
-		if(node.getEdges().size()==1)
-			return true;
-		return true;
-	}
-	
-	public GraphUndirected matchGraph(GraphUndirected graph)
-	{
-		NodeCpp node1 = null, node2 = null; 
-		ArrayList pathList = new ArrayList();
-		
-		
-		for (NodeCpp nodeItem : graph.getNodes()) 
-		{
-			if(node1 == null && nodeItem.isDegreeOdd())
-			{
-				node1 = nodeItem;
-				continue;
-			}
-			if(nodeItem.isDegreeOdd())
-			{	
-				node2 = nodeItem;
-			
-				pathList = getPathBetween(node1,node2);
-				graph = matchBetweenNodes(graph, pathList);
-				node1=null;
-				node2 = null;
-			}
-		}
-		
-		return graph;
-	}
-	
-	private GraphUndirected matchBetweenNodes(GraphUndirected graph, ArrayList pathList)
-	{
-		for(int i=0; i<pathList.size()-1; i++)
-		{
-			NodeCpp node1 = (NodeCpp) pathList.get(i);
-			NodeCpp node2 = (NodeCpp) pathList.get(i + 1);
-			
-			node1.connectWithNode(node2);
-		}
-		
-		return graph;
-	}
-	
-	public ArrayList getPathBetween(NodeCpp start, NodeCpp destination)
-	{
-		ArrayList pathList = new ArrayList();
-		
-		graph.resetStates();
-		pathList.add(start);
-		searchForPathBetweenNodesWithArrayList(start, destination, pathList);
-				
-		return pathList;
-		
-		
-	}
-	
-	private void searchForPathBetweenNodesWithArrayList(NodeCpp actual, NodeCpp destination, ArrayList pathList)
-	{
-		
-		for(int i=0; i<actual.getEdges().size(); i++)
-		{
-			
-		
-			for(int j=0; j<destination.getEdges().size(); j++)
-			{
-				if(!actual.getEdges().get(i).isVisited())
-				{
-					if(actual.getEdges().get(i).getRelatedNode(actual).equals(destination.getEdges().get(j).getRelatedNode(destination)))
-					{
-						pathList.add(actual.getEdges().get(i).getRelatedNode(actual));
-						pathList.add(destination);
-						return;
-					}
-					else
-					{
-						if(pathList.contains(actual.getEdges().get(i).getRelatedNode(actual)))
-						{
-							actual.getEdges().get(i).setVisited();
-							searchForPathBetweenNodesWithArrayList(actual.getEdges().get(i).getRelatedNode(actual),destination,pathList);
-							return;
-						}
-						else
-						{
-							pathList.add(actual.getEdges().get(i).getRelatedNode(actual));
-							actual.getEdges().get(i).setVisited();
-							searchForPathBetweenNodesWithArrayList(actual.getEdges().get(i).getRelatedNode(actual),destination,pathList);
-							return;
-						}
-						
-					}
-				}
-			}
-		}
-	}
-	
-	public ArrayList connectCircles(ArrayList big, ArrayList little)
-	{
-		// kleine wird zur großen hinzugefügt
-		ArrayList list = new ArrayList();
-		int sizeBig = big.size();
-		int sizeLittle= little.size();
 
-		for(int i=0; i<big.size(); i++)
-		{
-			list.add(big.get(i));
-		
-			if(big.get(i).equals(little.get(0)))
-			{
-				list.remove(i);						//damit doppeltes(aufgeschobenes) element gelöscht wird
-				
-				for(int j=0; j<little.size(); j++)
-				{
-					list.add(little.get(j));
-				}
-			}
-			
-		}
-		
-		return list;
-		
-	}
-	
-	public ArrayList getEulerianCircle()
-	{
-		NodeCpp node = graph.getNodes().iterator().next();
-		NodeCpp temp;
-		
-		ArrayList eulerianList = new ArrayList(getCircle(node));
+    public GraphUndirected matchGraph(GraphUndirected graph) {
+        NodeCpp node1 = null, node2 = null;
+        ArrayList<NodeCpp> pathList = new ArrayList<>();
 
+        for (final NodeCpp nodeItem : graph.getNodes()) {
+            if ((node1 == null) && nodeItem.isDegreeOdd()) {
+                node1 = nodeItem;
+                continue;
+            }
+            if (nodeItem.isDegreeOdd()) {
+                node2 = nodeItem;
 
-		
-		for(int i=0; i<eulerianList.size(); i++)			// gehe jedes element der Liste durch
-		{
-			temp = (NodeCpp) eulerianList.get(i);
+                pathList = getPathBetween(node1, node2);
+                graph = matchBetweenNodes(graph, pathList);
+                node1 = null;
+                node2 = null;
+            }
+        }
 
-			for(int j=0; j<temp.getEdges().size(); j++)			// gehe alle Kanten des aktuellen Elementes der Liste durch
-			{
-				if(!temp.getEdges().get(j).isVisited())				// wenn eine kante noch nicht besucht wurde...
-				{
-					ArrayList underGraph = new ArrayList(getCircle(temp));
-										
-					eulerianList = connectCircles(eulerianList, underGraph);
-					
-					i=0;						//beginne nochmal von vorn zu suchen
-					
-					
-				}
-			}
+        return graph;
+    }
 
-		}
-		
-		return eulerianList;
-	}
+    private GraphUndirected matchBetweenNodes(final GraphUndirected graph, final ArrayList<NodeCpp> pathList) {
+        for (int i = 0; i < (pathList.size() - 1); i++) {
+            final NodeCpp node1 = pathList.get(i);
+            final NodeCpp node2 = pathList.get(i + 1);
 
+            node1.connectWithNode(node2);
+        }
 
-	private ArrayList getCircle(NodeCpp node)
-	{	
-		NodeCpp node1 = node;
-		int i=0;
-		
-		ArrayList pathList = new ArrayList();
-		
-		for(int j=0; j<node.getEdges().size(); j++)
-		{
-			if(!node.getEdges().get(j).isVisited())
-			{
-				node1.getEdges().get(j).setVisited();
-				pathList.add(node1);
-				node1 = node1.getEdges().get(j).getRelatedNode(node1);
-				
-				break;
-			}
-		}
-		
-		do
-		{
-			
-			if(!node1.getEdges().get(i).isVisited())
-			{
-				node1.getEdges().get(i).setVisited();
-				pathList.add(node1);
-				node1 = node1.getEdges().get(i).getRelatedNode(node1);
-				i=0;
-			}
-			else
-			{
-				i++;				
-			}
+        return graph;
+    }
 
-		}while(!node1.equals(node));
+    public ArrayList<NodeCpp> getPathBetween(final NodeCpp start, final NodeCpp destination) {
+        final ArrayList<NodeCpp> pathList = new ArrayList<>();
 
-		pathList.add(node1);
-		
-		return pathList;
-		
-	}
+        graph.resetStates();
+        pathList.add(start);
+        searchForPathBetweenNodesWithArrayList(start, destination, pathList);
+
+        return pathList;
+
+    }
+
+    private void searchForPathBetweenNodesWithArrayList(final NodeCpp actual, final NodeCpp destination, final ArrayList<NodeCpp> pathList) {
+
+        for (int i = 0; i < actual.getEdges().size(); i++)
+            for (int j = 0; j < destination.getEdges().size(); j++)
+                if (!actual.getEdges().get(i).isVisited())
+                    if (actual.getEdges().get(i).getRelatedNode(actual).equals(destination.getEdges().get(j).getRelatedNode(destination))) {
+                        pathList.add(actual.getEdges().get(i).getRelatedNode(actual));
+                        pathList.add(destination);
+                        return;
+                    } else if (pathList.contains(actual.getEdges().get(i).getRelatedNode(actual))) {
+                        actual.getEdges().get(i).setVisited();
+                        searchForPathBetweenNodesWithArrayList(actual.getEdges().get(i).getRelatedNode(actual), destination, pathList);
+                        return;
+                    } else {
+                        pathList.add(actual.getEdges().get(i).getRelatedNode(actual));
+                        actual.getEdges().get(i).setVisited();
+                        searchForPathBetweenNodesWithArrayList(actual.getEdges().get(i).getRelatedNode(actual), destination, pathList);
+                        return;
+                    }
+    }
+
+    public ArrayList<NodeCpp> connectCircles(final ArrayList<NodeCpp> big, final ArrayList<NodeCpp> little) {
+        // kleine wird zur großen hinzugefügt
+        final ArrayList<NodeCpp> list = new ArrayList<>();
+
+        for (int i = 0; i < big.size(); i++) {
+            list.add(big.get(i));
+
+            if (big.get(i).equals(little.get(0))) {
+                list.remove(i); // damit doppeltes(aufgeschobenes) element gelöscht wird
+
+                for (int j = 0; j < little.size(); j++)
+                    list.add(little.get(j));
+            }
+
+        }
+
+        return list;
+
+    }
+
+    public ArrayList<NodeCpp> getEulerianCircle() {
+        final NodeCpp node = graph.getNodes().iterator().next();
+        NodeCpp temp;
+
+        ArrayList<NodeCpp> eulerianList = new ArrayList<>(getCircle(node));
+
+        for (int i = 0; i < eulerianList.size(); i++) // gehe jedes element der Liste durch
+        {
+            temp = eulerianList.get(i);
+
+            for (int j = 0; j < temp.getEdges().size(); j++)
+                if (!temp.getEdges().get(j).isVisited()) // wenn eine kante noch nicht besucht wurde...
+                {
+                    final ArrayList<NodeCpp> underGraph = new ArrayList<>(getCircle(temp));
+
+                    eulerianList = connectCircles(eulerianList, underGraph);
+
+                    i = 0; // beginne nochmal von vorn zu suchen
+
+                }
+
+        }
+
+        return eulerianList;
+    }
+
+    private ArrayList<NodeCpp> getCircle(final NodeCpp node) {
+        NodeCpp node1 = node;
+        int i = 0;
+
+        final ArrayList<NodeCpp> pathList = new ArrayList<>();
+
+        for (int j = 0; j < node.getEdges().size(); j++)
+            if (!node.getEdges().get(j).isVisited()) {
+                node1.getEdges().get(j).setVisited();
+                pathList.add(node1);
+                node1 = node1.getEdges().get(j).getRelatedNode(node1);
+
+                break;
+            }
+
+        do
+            if (!node1.getEdges().get(i).isVisited()) {
+                node1.getEdges().get(i).setVisited();
+                pathList.add(node1);
+                node1 = node1.getEdges().get(i).getRelatedNode(node1);
+                i = 0;
+            } else
+                i++;
+        while (!node1.equals(node));
+
+        pathList.add(node1);
+
+        return pathList;
+
+    }
 
 }
--- src/test/java/edu/uaskl/cpp/model/GraphVollstaendigTest.java
+++ src/test/java/edu/uaskl/cpp/model/GraphVollstaendigTest.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 
 import org.junit.Test;
 
-import edu.uaskl.cpp.model.edge.EdgeCpp;
 import edu.uaskl.cpp.model.graph.GraphComplete;
 import edu.uaskl.cpp.model.graph.GraphUndirected;
 import edu.uaskl.cpp.model.node.NodeCpp;
@@ -19,8 +18,18 @@ public class GraphVollstaendigTest {
     }
 
     @Test
+    public void hasEulerCircleTrue() {
+        assertThat(new GraphComplete(19).getAlgorithms().hasEulerCircle()).isTrue();
+    }
+
+    @Test
     public void hasEulerCircleFalse() {
-        assertThat(new GraphComplete(20).getAlgorithms().hasEulerCircle()).isTrue();
+        assertThat(new GraphComplete(20).getAlgorithms().hasEulerCircle()).isFalse();
+    }
+
+    @Test
+    public void isConnected4True() {
+        assertThat(new GraphComplete(4).getAlgorithms().isConnected()).isTrue();
     }
 
     @Test
@@ -35,13 +44,13 @@ public class GraphVollstaendigTest {
 
     @Test
     public void isConnectedFalse() {
-        GraphUndirected graph = new GraphUndirected();
-        NodeCpp node1 = new NodeCpp();
-        NodeCpp node2 = new NodeCpp();
-        NodeCpp node3 = new NodeCpp();
-        NodeCpp node4 = new NodeCpp();
-        NodeCpp node5 = new NodeCpp();
-        NodeCpp node6 = new NodeCpp();
+        final GraphUndirected graph = new GraphUndirected();
+        final NodeCpp node1 = new NodeCpp();
+        final NodeCpp node2 = new NodeCpp();
+        final NodeCpp node3 = new NodeCpp();
+        final NodeCpp node4 = new NodeCpp();
+        final NodeCpp node5 = new NodeCpp();
+        final NodeCpp node6 = new NodeCpp();
         node1.connectWithNode(node2);
         node3.connectWithNode(node3);
         node4.connectWithNode(node5);
@@ -51,19 +60,17 @@ public class GraphVollstaendigTest {
         graph.addNode(node1).addNode(node2).addNode(node3).addNode(node4).addNode(node5).addNode(node6);
         assertThat(graph.getAlgorithms().isConnected()).isFalse();
     }
-    
+
     @Test
-    public void matchingTest()
-    {
-        GraphUndirected graph = new GraphUndirected();
-        NodeCpp node1 = new NodeCpp();
-        NodeCpp node2 = new NodeCpp();
-        NodeCpp node3 = new NodeCpp();
-        NodeCpp node4 = new NodeCpp();
-        NodeCpp node5 = new NodeCpp();
-        NodeCpp node6 = new NodeCpp();
-        
-        
+    public void matchingTest() {
+        final GraphUndirected graph = new GraphUndirected();
+        final NodeCpp node1 = new NodeCpp();
+        final NodeCpp node2 = new NodeCpp();
+        final NodeCpp node3 = new NodeCpp();
+        final NodeCpp node4 = new NodeCpp();
+        final NodeCpp node5 = new NodeCpp();
+        final NodeCpp node6 = new NodeCpp();
+
         node1.connectWithNode(node2);
         node2.connectWithNode(node3);
         node2.connectWithNode(node4);
@@ -72,43 +79,36 @@ public class GraphVollstaendigTest {
         node3.connectWithNode(node6);
         node4.connectWithNode(node5);
         node5.connectWithNode(node6);
-        
+
         graph.addNode(node1).addNode(node2).addNode(node3).addNode(node4).addNode(node5).addNode(node6);
-        
+
         graph.getAlgorithms().matchGraph(graph);
-        
+
         boolean odd = false;
-        
-		for (NodeCpp nodeItem : graph.getNodes()) 
-		{
-			if(nodeItem.isDegreeOdd())
-				{
-					odd = true;
-					break;
-				}
-			
-		}
-	
-    	assertThat(odd).isFalse();
+
+        for (final NodeCpp nodeItem : graph.getNodes())
+            if (nodeItem.isDegreeOdd()) {
+                odd = true;
+                break;
+            }
+
+        assertThat(odd).isFalse();
     }
-    
+
     @Test
-    public void getPathBetweenTest()
-    {
-    	GraphUndirected graph = new GraphUndirected();
-    
-        NodeCpp node0 = new NodeCpp();
-        NodeCpp node1 = new NodeCpp();
-        NodeCpp node2 = new NodeCpp();
-        NodeCpp node3 = new NodeCpp();
-        NodeCpp node4 = new NodeCpp();
-        NodeCpp node5 = new NodeCpp();
-        NodeCpp node6 = new NodeCpp();
-        NodeCpp node7 = new NodeCpp();
-        NodeCpp node8 = new NodeCpp();
-        
-        
-        
+    public void getPathBetweenTest() {
+        final GraphUndirected graph = new GraphUndirected();
+
+        final NodeCpp node0 = new NodeCpp();
+        final NodeCpp node1 = new NodeCpp();
+        final NodeCpp node2 = new NodeCpp();
+        final NodeCpp node3 = new NodeCpp();
+        final NodeCpp node4 = new NodeCpp();
+        final NodeCpp node5 = new NodeCpp();
+        final NodeCpp node6 = new NodeCpp();
+        final NodeCpp node7 = new NodeCpp();
+        final NodeCpp node8 = new NodeCpp();
+
         node0.connectWithNode(node1);
         node1.connectWithNode(node2);
         node1.connectWithNode(node5);
@@ -123,97 +123,85 @@ public class GraphVollstaendigTest {
         node5.connectWithNode(node6);
         node6.connectWithNode(node7);
         node7.connectWithNode(node8);
-        
+
         graph.addNode(node0).addNode(node1).addNode(node2).addNode(node3).addNode(node4).addNode(node5).addNode(node6).addNode(node7).addNode(node8);
-   
-        ArrayList pathList = graph.getAlgorithms().getPathBetween(node0, node8);
 
-        
-        assertThat(pathList.get(0).equals(node0)&& pathList.get(pathList.size()-1).equals(node8)).isFalse();
+        final ArrayList pathList = graph.getAlgorithms().getPathBetween(node0, node8);
+
+        assertThat(pathList.get(0).equals(node0) && pathList.get(pathList.size() - 1).equals(node8)).isFalse();
     }
-    
-	
-	@Test
-	public void connectCirclesTest()
-	{
-		GraphUndirected graph = new GraphUndirected();
-		
-		NodeCpp node0 = new NodeCpp();
-	    NodeCpp node1 = new NodeCpp();
-	    NodeCpp node2 = new NodeCpp();
-	    NodeCpp node3 = new NodeCpp();
-	    NodeCpp node4 = new NodeCpp();
-	    
-	    
-	    node0.connectWithNode(node1);
-	    node1.connectWithNode(node2);
-	    node2.connectWithNode(node0);
-	    node1.connectWithNode(node2);
-	    node1.connectWithNode(node3);
-	    node3.connectWithNode(node4);
-	    node4.connectWithNode(node2);
-	    
-	    graph.addNode(node0).addNode(node1).addNode(node2).addNode(node3).addNode(node4);
-	    
-	    ArrayList little = new ArrayList();
-	    ArrayList big = new ArrayList();
-	    
-	    big.add(node0);
-	    big.add(node1);
-	    big.add(node2);
-	    big.add(node0);
-	    
-	    little.add(node1);
-	    little.add(node3);
-	    little.add(node4);
-	    little.add(node2);
-	    little.add(node1);
-	    
-	   
-	    ArrayList list = new ArrayList (graph.getAlgorithms().connectCircles(big, little));
-	    
-	    assertThat(list.get(0).equals(list.get(list.size()-1))).isTrue();
-	}
-	
-	@Test
-	public void getEulerianCircleTEST()
-	{
-		GraphUndirected graph = new GraphUndirected();
 
-		NodeCpp node0 = new NodeCpp();
-	    NodeCpp node1 = new NodeCpp();
-	    NodeCpp node2 = new NodeCpp();
-	    NodeCpp node3 = new NodeCpp();
-	    NodeCpp node4 = new NodeCpp();
-	    
-	    
-	    node0.connectWithNode(node1);
-	    node1.connectWithNode(node2);
-	    node2.connectWithNode(node0);
-	    node2.connectWithNode(node1);
-	    node1.connectWithNode(node3);
-	    node3.connectWithNode(node4);
-	    node4.connectWithNode(node2);
-	    
-	    graph.addNode(node0).addNode(node1).addNode(node2).addNode(node3).addNode(node4);
+    @Test
+    public void connectCirclesTest() {
+        final GraphUndirected graph = new GraphUndirected();
 
-		ArrayList eulerianList = new ArrayList(graph.getAlgorithms().getEulerianCircle());
-		
-		
-		
-		boolean odd = false;
-        
-		for (NodeCpp nodeItem : graph.getNodes()) 
-		{
-			if(nodeItem.isDegreeOdd())
-				{
-					odd = true;
-					break;
-				}
-		}
-	    
-		assertThat(odd).isFalse();
-		assertThat(eulerianList.get(0).equals(eulerianList.get(eulerianList.size()-1))).isTrue();
-	}
+        final NodeCpp node0 = new NodeCpp();
+        final NodeCpp node1 = new NodeCpp();
+        final NodeCpp node2 = new NodeCpp();
+        final NodeCpp node3 = new NodeCpp();
+        final NodeCpp node4 = new NodeCpp();
+
+        node0.connectWithNode(node1);
+        node1.connectWithNode(node2);
+        node2.connectWithNode(node0);
+        node1.connectWithNode(node2);
+        node1.connectWithNode(node3);
+        node3.connectWithNode(node4);
+        node4.connectWithNode(node2);
+
+        graph.addNode(node0).addNode(node1).addNode(node2).addNode(node3).addNode(node4);
+
+        final ArrayList little = new ArrayList();
+        final ArrayList big = new ArrayList();
+
+        big.add(node0);
+        big.add(node1);
+        big.add(node2);
+        big.add(node0);
+
+        little.add(node1);
+        little.add(node3);
+        little.add(node4);
+        little.add(node2);
+        little.add(node1);
+
+        final ArrayList list = new ArrayList(graph.getAlgorithms().connectCircles(big, little));
+
+        assertThat(list.get(0).equals(list.get(list.size() - 1))).isTrue();
+    }
+
+    @Test
+    public void getEulerianCircleTEST() {
+        final GraphUndirected graph = new GraphUndirected();
+
+        final NodeCpp node0 = new NodeCpp();
+        final NodeCpp node1 = new NodeCpp();
+        final NodeCpp node2 = new NodeCpp();
+        final NodeCpp node3 = new NodeCpp();
+        final NodeCpp node4 = new NodeCpp();
+
+        node0.connectWithNode(node1);
+        node1.connectWithNode(node2);
+        node2.connectWithNode(node0);
+        node2.connectWithNode(node1);
+        node1.connectWithNode(node3);
+        node3.connectWithNode(node4);
+        node4.connectWithNode(node2);
+
+        graph.addNode(node0).addNode(node1).addNode(node2).addNode(node3).addNode(node4);
+
+        final ArrayList eulerianList = new ArrayList(graph.getAlgorithms().getEulerianCircle());
+
+        boolean odd = false;
+
+        for (final NodeCpp nodeItem : graph.getNodes())
+            if (nodeItem.isDegreeOdd()) {
+                odd = true;
+                break;
+            }
+
+        assertThat(odd).isFalse();
+        assertThat(eulerianList.get(0).equals(eulerianList.get(eulerianList.size() - 1))).isTrue();
+    }
 
 }
