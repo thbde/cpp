10a149c250c717c2c262ff0e911f691ccacd832e
Erik Müller <ermu0001@stud.fh-kl.de>
2013-11-09 16:21:34 +0100

    Make paths generic.

--- src/main/java/edu/uaskl/cpp/model/path/interfaces/Path.java
+++ src/main/java/edu/uaskl/cpp/model/path/interfaces/Path.java
@@ -2,7 +2,8 @@ package edu.uaskl.cpp.model.path.interfaces;
 
 import java.util.List;
 
-import edu.uaskl.cpp.model.node.NodeCpp;
+import edu.uaskl.cpp.model.edge.EdgeExtended;
+import edu.uaskl.cpp.model.node.NodeExtended;
 
 /**
  * A path is an ordered list of nodes together with a distance.
@@ -10,10 +11,10 @@ import edu.uaskl.cpp.model.node.NodeCpp;
  * @author tbach
  */
 
-public interface Path {
+public interface Path<T extends NodeExtended<T, V>, V extends EdgeExtended<T, V>> {
     public int getDistance();
 
     public int getLength();
 
-    public List<NodeCpp> getNodes();
+    public List<T> getNodes();
 }
